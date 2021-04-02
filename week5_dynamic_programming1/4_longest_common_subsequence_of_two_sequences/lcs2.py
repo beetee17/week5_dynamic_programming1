af45d32b74c22dcf916e0179ea765acb594a353e
beetee17 <brandon.thio@yahoo.com.sg>
2021-04-03 02:15:13 +0800

    init

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -3,8 +3,76 @@
 import sys
 
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    """Compute the length of a longest common subsequence of two sequences.
+
+    Task
+    Given two sequences 𝐴 = (𝑎1,𝑎2,...,𝑎𝑛) and 𝐵 = (𝑏1,𝑏2,...,𝑏𝑚), find the length
+    of their longest common subsequence, i.e., the largest non-negative integer 𝑝
+    such that there exist indices 1 ≤ 𝑖1 < 𝑖2 <···< 𝑖𝑝 ≤ 𝑛 and 1 ≤ 𝑗1 < 𝑗2 < ··· < 𝑗𝑝 ≤ 𝑚,
+    such that 𝑎𝑖1 = 𝑏𝑗1, ... , 𝑎𝑖𝑝 = 𝑏𝑗𝑝.
+
+    Input Format
+    First line: 𝑛 
+    Second line: 𝑎1, 𝑎2, . . . , 𝑎𝑛 
+    Third line: 𝑚
+    Fourth line: 𝑏1, 𝑏2, . . . , 𝑏𝑚
+
+    Constraints
+    1 ≤ 𝑛,
+    𝑚 ≤ 100 
+    −10^9 < 𝑎𝑖, 𝑏𝑖 < 10^9  
+    
+    Output Format 
+    Output 𝑝."""
+
+
+    T = [[0 for j in range(len(a) + 1)] for i in range(len(b) + 1)] 
+    
+    # iteratively populate table by finding max at each square
+    for j in range(1, len(a) + 1):
+        for i in range(1, len(b) + 1):
+            if b[i-1] == a[j-1]:
+                # extend subsequence if matching characters
+                T[i][j] = T[i-1][j-1] + 1
+
+            else:
+                # get max of 2 previous possibilities
+                T[i][j] = max(T[i-1][j], T[i][j-1])
+
+    s = backtrack(T, a, b)
+    print(s)
+    # return max length of subsequence between the 2 sequences
+    return T[-1][-1]
+
+
+def backtrack(T, a, b, S = []):
+
+    # backtracking (works to find single lcs only)
+
+    s = []
+    i, j = len(b), len(a) 
+
+    while i > 0 and j > 0:
+        print(i,j)
+        if b[i-1] == a[j-1]:
+            s.append(a[j-1])
+            i -= 1
+            j -= 1
+        else:
+            if T[i-1][j] > T[i][j-1]:
+                i -= 1
+            elif T[i][j-1] > T[i-1][j]:
+                j -= 1
+
+            else:
+                backtrack(T, a[:j], b[:i-1], S)
+                print("branching out")
+                j -= 1
+
+    s = s[::-1]
+    S.append(s)
+    return s
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,10 +1,55 @@
 #Uses python3
 
+from itertools import permutations
 import sys
 
+
+
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    """"""
+
+
+    
+    # Approach to problem: find all lcs of a and b 
+    # s1, s2... = lcs(a, b), lcs(a, b, c) = max_len(lcs(s1, c), lcs(s2, c),....)
+    # does not work as too time consuming
+    # instead, simply extend 2D lcs table to 3D matrix
+
+    T = [[[0 for k in range(len(c) + 1)] for j in range(len(b) + 1)] for i in range(len(a) + 1)]
+
+    # iteratively populate matrix by finding max at each square
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            for k in range(1, len(c) + 1):
+                if a[i-1] == b[j-1] and b[j-1] == c[k-1]:
+                    # extend subsequence if matching characters
+                    T[i][j][k] = T[i-1][j-1][k-1] + 1
+
+                else:
+                    # get max of 3 possiblities
+                    T[i][j][k] = max(T[i-1][j][k], T[i][j-1][k], T[i][j][k-1])
+                                  
+    # print(T)
+    return T[-1][-1][-1]
+
+
+
+# def lcs3(a, b, c):
+#     m = len(a)
+#     l = len(b)
+#     n = len(c)
+#     subs = [[[0 for k in range(n+1)] for j in range(l+1)] for i in range(m+1)]
+
+#     for i, x in enumerate(a):
+#         for j, y in enumerate(b):
+#             for k, z in enumerate(c):
+#                 if x == y and y == z:
+#                     subs[i+1][j+1][k+1] = subs[i][j][k] + 1
+#                 else:
+#                     subs[i+1][j+1][k+1] = max(subs[i+1][j+1][k], 
+#                                               subs[i][j+1][k+1], 
+#                                               subs[i+1][j][k+1])
+#     return subs[-1][-1][-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -21,3 +66,23 @@ if __name__ == '__main__':
     data = data[1:]
     c = data[:cn]
     print(lcs3(a, b, c))
+
+    ## STRESS TEST
+    # from itertools import combinations
+    # a = [3,1,2,5,7]
+    # b = [3,2,6,7]
+    # c = [3,1,2,6,7]
+    # answer = 0
+    # largest_subseq = []
+    # for i in range(1, min(len(a), len(b), len(c)) + 1):
+    # # enumerate all subsequences of a,b and c of length i
+    #     for x in combinations(a, i):
+    #         for y in combinations(b, i):
+    #             for z in combinations(c, i):
+    #                 if x == y and y == z:
+    #                     # if the sequence is common for a,b and c, update the answer
+    #                     largest_subseq = list(x)
+    #                     answer = i
+            
+    # print(answer, largest_subseq)
+
